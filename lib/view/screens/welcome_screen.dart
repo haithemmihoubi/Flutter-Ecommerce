0f4909b43491762bd627f653237a9baa5cc66bd8
Haithem Mihoubi <64318421+haithemmihoubi@users.noreply.github.com>
2022-03-04 02:03:56 +0100

    initialising project structure

---
+++ lib/view/screens/welcome_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(child: Text("welcome screen")),
+    );
+  }
+}
