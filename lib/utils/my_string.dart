a467f8e5caab0a02215c8c56da22501ed38b5d2d
Haithem Mihoubi <64318421+haithemmihoubi@users.noreply.github.com>
2022-03-05 20:56:31 +0100

    adding base url

--- lib/utils/my_string.dart
+++ lib/utils/my_string.dart
@@ -1,6 +1,7 @@
+//  regular expression for validating emails
 String validationEmail =
     r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+";
-
+//  regular expression for validating String only
 String validationName = r'^[a-z A-Z]+$';
-
+//   The baseUrl  of the current api
 String baseUrl = 'https://fakestoreapi.com';
