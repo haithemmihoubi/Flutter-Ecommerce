b824f66644398ad40972b4026511fde7ab91f3ef
Haithem Mihoubi <64318421+haithemmihoubi@users.noreply.github.com>
2022-03-07 15:05:12 +0100

    checkBox functionality implementation

--- lib/logic/controller/auth_controller.dart
+++ lib/logic/controller/auth_controller.dart
@@ -2,16 +2,16 @@ import 'package:get/get.dart';
 
 class AuthController extends GetxController {
   bool isVisible = false;
-  bool ischecked = false;
+  bool isChecked = false;
 
   void isvisibility() {
     isVisible = !isVisible;
     // update  the state like notifyListener => Listen  to  updates in this method
     update();
   }
-
-  void ischeckedBox() {
-    ischecked = !ischecked;
+// checkBox functionality implementation
+  void isCheckedBox() {
+    isChecked = !isChecked;
     // update  the state like notifyListener => Listen  to  updates in this method
     update();
   }
